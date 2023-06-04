58fcd6d5bb39303389ac6a14e07d7c0e33114113
MattSperry <matthewjoelsperry@gmail.com>
2023-06-04 10:18:28 -0600

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Matt Sperry on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
